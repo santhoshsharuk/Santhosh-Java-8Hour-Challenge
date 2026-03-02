9a136e1398131052349d4350c5ed0cc4af872600
santhoshsharuk <santhoshsharuk16@gmail.com>
2026-03-02 11:54:05 +0530

    Add Java program for calculating the average of three user-input integers

---
+++ Day 02/Q&A section/q3.java
@@ -0,0 +1,18 @@
+	import java.lang.System;
+import java.util.Scanner;
+
+class q3{
+    
+    public static void main(String args[])
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int a=sc.nextInt();
+	  int b=sc.nextInt();
+	  int c=sc.nextInt();
+	  int d=a*b*c;
+	  int e=a+b+c;
+	  System.out.print(d/e);
+	
+
+	}
+}
