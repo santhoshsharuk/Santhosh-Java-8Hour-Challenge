5fe288e467bb362b498d42c91ebe5a8127f0b884
santhoshsharuk <santhoshsharuk16@gmail.com>
2026-03-03 09:41:05 +0530

    taking input from user

--- Day 03/if else/q1.java
+++ Day 03/if else/q1.java
@@ -1,11 +1,32 @@
 import java.lang.System;
 import java.util.Scanner;
 
+
+/* 
 class cup{
     public static void main (String arg[])
 
     {
         String a="win";
+        if (a.equals("win"))
+        {
+            System.out.println("Ee Sala Cup Namadey");
+        }
+        else
+        {
+            System.out.println("Cup Illa");
+        }
+    }
+}
+    */
+
+//taking input from user
+class cup{
+    public static void main (String arg[])
+
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
         if (a.equals("win"))
         {
             System.out.println("Ee Sala Cup Namadey");
